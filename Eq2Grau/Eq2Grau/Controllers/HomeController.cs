3de878e4b34bcdbb67ad811d11a536124ee01dc5
Joot111 <joaoamoroso04@gmail.com>
2024-03-05 10:55:50 +0000

    Commit Intermediário

--- Eq2Grau/Eq2Grau/Controllers/HomeController.cs
+++ Eq2Grau/Eq2Grau/Controllers/HomeController.cs
@@ -13,8 +13,38 @@ namespace Eq2Grau.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        /// <summary>
+        /// Portal de entrada dos pedidos feitos à consola 
+        /// Método para calcular as raízes de uma equação de 2º grau
+        /// </summary>
+        /// <param name="A">parâmetro do x^2</param>
+        /// <param name="B">parâmetro do x</param>
+        /// <param name="C">parâmtro independente</param>
+        /// <returns></returns>
+
+        public IActionResult Index(string A, string B, string C)
         {
+            /* ALGORITMO:
+             * 1- ler parâmetros A, B, C
+             * 2- validar se se pode fazer operações com eles
+             *  2.1- têm de ser números
+             *  2.2- A != 0
+             * 3- se posso calcular,
+             *  3.1- determinar as raízes
+             *      x1=( -b +/- (sqrt(b^2 -4ac))/(2a)
+             *      3.1.1- calcular o delta: b^2-4ac
+             *      3.1.2- se delta > 0, há 2 raízes reais distintas 
+             *                          x1 e x2
+             *             se delta = 0, há 2 raízes reais distintas 
+             *                          x1 = x2
+             *             se delta < 0, há 2 raízes complexas conjugadas
+             *                          x1 = -b/(2a) + sqrt(-delta/(2a) i
+             *                          x2 = -b/(2a) - sqrt(-delta/(2a) i
+             *  3.2- enviar a resposta para o cliente
+             *  se não posso calcular (else)
+             *      enviar mensagens de erro para o cliente (else)
+             */
+
             return View();
         }
 
